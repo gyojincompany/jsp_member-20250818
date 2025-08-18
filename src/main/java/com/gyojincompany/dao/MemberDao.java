737c10f0883629cead1301f07668b829c8e9b03f
gyojincompany <luckyred@gmail.com>
2025-08-18 16:23:13 +0900

    회원 로그인 기능 구현 추가

--- src/main/java/com/gyojincompany/dao/MemberDao.java
+++ src/main/java/com/gyojincompany/dao/MemberDao.java
@@ -20,6 +20,8 @@ public class MemberDao {
 	
 	public static final int MEMBER_JOIN_SUCCESS = 1;
 	public static final int MEMBER_JOIN_FAIL = 0;	
+	public static final int LOGIN_SUCCESS = 1;
+	public static final int LOGIN_FAIL = 0;
 	
 	public int insertMember(MemberDto memberDto) { //회원 가입 메서드
 		
@@ -63,8 +65,55 @@ public class MemberDao {
 			return MEMBER_JOIN_SUCCESS; //1
 		} else {
 			return MEMBER_JOIN_FAIL; //0
-		}
+		}		
 		
+	}
+	
+	public int loginCheck(String id, String pw) { //로그인 성공 여부 반환하는 메서드
+		
+		String sql = "SELECT * FROM membertbl WHERE memberid=? AND memberpw=?";
+		
+		int sqlResult = 0;
+		
+		try {
+			Class.forName(driverName); //MySQL 드라이버 클래스 불러오기			
+			conn = DriverManager.getConnection(url, username, password);
+			//커넥션이 메모리 생성(DB와 연결 커넥션 conn 생성)
+			
+			pstmt = conn.prepareStatement(sql); //pstmt 객체 생성(sql 삽입)
+			pstmt.setString(1, id);
+			pstmt.setString(2, pw);			
+			
+			rs = pstmt.executeQuery(); //성공하면 sqlResult 값이 1로 변환
+			// SQL문을 DB에서 실행->성공하면 1이 반환, 실패면 1이 아닌 값 0이 반환
+			
+			if(rs.next()) { //로그인 성공
+				sqlResult = LOGIN_SUCCESS;
+				
+			} else { //로그인 실패
+				sqlResult = LOGIN_FAIL;
+			}
+			
+			
+		} catch (Exception e) {
+			System.out.println("DB 에러 발생! 로그인 체크 실패!");
+			e.printStackTrace(); //에러 내용 출력
+		} finally { //에러의 발생여부와 상관 없이 Connection 닫기 실행 
+			try {
+				if(rs != null) { //rs가 null 이 아니면 닫기(stmt 닫기 보다 먼저 실행)
+					rs.close();
+				}				
+				if(pstmt != null) { //stmt가 null 이 아니면 닫기(conn 닫기 보다 먼저 실행)
+					pstmt.close();
+				}				
+				if(conn != null) { //Connection이 null 이 아닐 때만 닫기
+					conn.close();
+				}
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return sqlResult; //1로 반환되면 로그인 성공, 0으로 반환되면 로그인 실패
 		
 	}
 
